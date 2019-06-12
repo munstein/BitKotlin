57795b5fdccbf267f09c03111230cb1eaf91b83c
munstein <eduardo.munstein@live.com>
2019-06-11 22:32:28 -0300

    Migrates to AndroidX

--- app/src/main/java/com/munstein/bitkotlin/base/BaseActivity.kt
+++ app/src/main/java/com/munstein/bitkotlin/base/BaseActivity.kt
@@ -1,6 +1,6 @@
 package com.munstein.bitkotlin.base
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * Created by @Munstein on 03/01/2018. --21:33
